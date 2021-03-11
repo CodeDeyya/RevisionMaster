2a935b851142826122e4eae4795f59b5b99aaeca
CodeDeyya <CodeDeyya@gmail.com>
2021-03-11 13:21:56 +0530

    Test

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -3,9 +3,14 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 // @material-ui/icons
-import Chat from "@material-ui/icons/Chat";
-import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Fingerprint from "@material-ui/icons/Fingerprint";
+
+import AddBoxRoundedIcon from '@material-ui/icons/AddBoxRounded';
+import GamesRoundedIcon from '@material-ui/icons/GamesRounded';
+import LocalHospitalRoundedIcon from '@material-ui/icons/LocalHospitalRounded';
+import AccountBalanceRoundedIcon from '@material-ui/icons/AccountBalanceRounded';
+import SortByAlphaRoundedIcon from '@material-ui/icons/SortByAlphaRounded';
+import WavesRoundedIcon from '@material-ui/icons/WavesRounded';
+
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -21,13 +26,9 @@ export default function ProductSection() {
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Let{"'"}s talk product</h2>
+          <h2 className={classes.title}>Online Revision Kit</h2>
           <h5 className={classes.description}>
-            This is the paragraph where you can write more details about your
-            product. Keep you user engaged by providing meaningful information.
-            Remember that by this time, the user is curious, otherwise he wouldn
-            {"'"}t scroll to get here. Add a button if you want the user to see
-            more.
+          A level Revision සඳහා අප හා එක් වන්න. කාලානුරූපී MCQ ප්‍රශ්න කරන්න. Improve your subject knowledge and time management skills. We provide revision for Maths, Physics, Chemistry, Economics and English. අපගේ ප්‍රශ්න පදනම් වී ඇත්තේ අතීත පත්‍රිකා මත ය (past papers). Watch the video above to learn how to use.
           </h5>
         </GridItem>
       </GridContainer>
@@ -35,28 +36,55 @@ export default function ProductSection() {
         <GridContainer>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title="Free Chat"
-              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-              icon={Chat}
-              iconColor="info"
+              title="Biology"
+              description="Biology is a Science which is focused on studying of living organisms. Biology is a subject which is very complex and vast. Hence for the convenience of studying, it has been divided into three primary braches: Zoology (the study of animals), Botany (study of plants) and Microbiology(study of microorganisms)."
+              icon={LocalHospitalRoundedIcon}
+              iconColor="primary"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title="Verified Users"
+              title="Econ"
               description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-              icon={VerifiedUser}
+              icon={AccountBalanceRoundedIcon}
               iconColor="success"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title="Fingerprint"
+              title="Maths"
               description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-              icon={Fingerprint}
+              icon={AddBoxRoundedIcon}
               iconColor="danger"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Physics"
+              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+              icon={GamesRoundedIcon}
+              iconColor="warning"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="English"
+              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+              icon={SortByAlphaRoundedIcon}
+              iconColor="rose"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Chemistry"
+              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+              icon={WavesRoundedIcon}
+              iconColor="info"
               vertical
             />
           </GridItem>
